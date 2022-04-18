250dc89fcc23f268201c9d8014b69f506d408ca3
lahuya <103897171+lahuya@users.noreply.github.com>
2022-04-18 20:24:32 +0700

    Update SPEEDGRACE.php

--- SPEEDGRACE.php
+++ SPEEDGRACE.php
@@ -1,1 +1,19 @@
-ghh
+{"Version":"1.1",
+"Url":"https://github.com/SPEED-GRACE/ROUY-VPN/raw/main/ruay.zip" ,
+"Changelog":"SPEED GRACE
+รายละเอียดภายในแอป
+แก้ไขรายละเอียดครับ 
+ทดลอง",
+ 
+
+ 
+ 
+ 
+
+"latestVersion": "1.0.0",
+"latestVersionCode": "1",
+"url": "",
+ "releaseNotes": [
+ "-Demo"
+  ]
+}
